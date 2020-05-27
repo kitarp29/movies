bea265278335c811847163e97d90383989e6c847
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-05-27 19:34:52 +0530

    started frontend for index.html

--- pk/app.js
+++ pk/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+app.use(express.static("public"));
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 })
