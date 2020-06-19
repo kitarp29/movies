1da9b581807c07b69555c5c2bb40b0e8ec52b93a
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-06-19 20:11:29 +0530

    env and gitigonre se heroku ma kaam nhi ho rha tha

--- pk/app.js
+++ pk/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
-const omdb = new(require('omdbapi'))(process.env.API_KEY);
+const omdb = new(require('omdbapi'))('e343ee42');
 const ejsLint = require('ejs-lint');
 var path = require("path");
 app.set('view engine', 'ejs');
